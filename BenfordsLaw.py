9fa49e996af013b0b018f7e7215d116881343337
jrojas-pena <83971859+jrojas-pena@users.noreply.github.com>
2022-12-26 11:10:31 -0500

    Update BenfordsLaw.py
    Optimization to get the first digit of the number. When the number gets too big it gets too slow by trying to convert it to a string.
    For the first million Fibonacci numbers it gets a boost in performance from more than 24 hours to around 11 minutes.

--- BenfordsLaw.py
+++ BenfordsLaw.py
@@ -1,39 +1,46 @@
 from datetime import datetime
+import math
 
-def fibonacci(num):    
-    count = 0
-    n1, n2 = 1, 1
-    nth = 0
-    if num < 0:
-        return 1
-    while count < num:
-        nth = n1 + n2
-        n1 = n2
-        n2 = nth
-        count += 1    
-    return n1
+def fibonacci(arr, num):
+    start2 = datetime.now()
+    arr.append(1)
+    arr.append(1)
+    for i in range(2, num):
+        arr.append(arr[i-2] + arr[i -1])    
+    end2 = datetime.now()
+    print("On fibonacci funct %.03f"%(end2 - start2).total_seconds())
+    return arr
+
+    
 
 def countFirstDigit(arr):
     Dict = {}
-    for item in arr:         
-        string = item[0]
-        if string in Dict:
-            Dict[string] += 1
+    start1 = datetime.now()
+    for item in arr:    
+        firstDigit = first_n_digits(item, 1)
+        if firstDigit in Dict:
+            Dict[firstDigit] += 1
         else:
-            Dict[string] = 1
+            Dict[firstDigit] = 1
+    end1 = datetime.now()
+    print("On first Digit funct %.03f"%(end1 - start1).total_seconds())
     Dict = sorted(Dict.items(), key=lambda x: x[1], reverse=True)
     for i in Dict:
-        print("% s : % d"%(i[0], i[1]))
+        print("% d : % d"%(i[0], i[1]))
+
+def first_n_digits(num, n):
+    return num // 10 ** (int(math.log10(num)) - n + 1)
+    
 
 
 arrFib = []
 
 num = int(input("Enter a number:"))
 start = datetime.now()
-for i in range(0, num):
-    arrFib.append(str(fibonacci(i)))
-end2 = datetime.now()
-print("On fibonacci funct %.03f"%(end2 - start).total_seconds())
+
+arrFib = fibonacci(arrFib, num)
+#print(arrFib[num-1])
+
 #print(arrFib)
 countFirstDigit(arrFib)
 end = datetime.now()
