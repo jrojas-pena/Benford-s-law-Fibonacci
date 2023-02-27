3af97161d15c71cb5f29bf1e9f8265bfc335ca4c
Juan <rojasjuandiego6@gmail.com>
2023-02-27 11:21:36 -0500

    Add graphing capabilities

--- BenfordsLaw.py
+++ BenfordsLaw.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 import math
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import colors
-from matplotlib.ticker import PercentFormatter
+#from matplotlib import colors
+#from matplotlib.ticker import PercentFormatter
 
 
 # Will return an array with the numbers in the Fibonacci sequence of size of num
@@ -20,24 +20,33 @@ def fibonacci(arr, num):
     
 # Counts the amount of times a specific digit appears as the first digit on the numbers inside the array (arr)
 def countFirstDigit(arr):
-    Dict = {}
+    # Dictionary of digits
+    digitsDict = {}
+
     start1 = datetime.now()
     for item in arr:    
         firstDigit = first_n_digits(item, 1)
-        if firstDigit in Dict:
-            Dict[firstDigit] += 1
+        if firstDigit in digitsDict:
+            digitsDict[firstDigit] += 1
         else:
-            Dict[firstDigit] = 1
+            digitsDict[firstDigit] = 1
     end1 = datetime.now()
     print("On first Digit funct %.03f"%(end1 - start1).total_seconds())
-    Dict = sorted(Dict.items(), key=lambda x: x[1], reverse=True)
-    for i in Dict:
+    digitsDict = sorted(digitsDict.items(), key=lambda x: x[1], reverse=True)
+    for i in digitsDict:
         print("% d : % d"%(i[0], i[1]))
+    return digitsDict
 
 # Returns the first n digits in num
 def first_n_digits(num, n):
     return num // 10 ** (int(math.log10(num)) - n + 1)
-    
+
+def graphResults(digitsDict):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.bar(range(1, 10), list(map(lambda x: x[1], digitsDict)), color='tab:blue')
+
+    plt.show()
 
 
 arrFib = []
@@ -49,7 +58,7 @@ arrFib = fibonacci(arrFib, num)
 #print(arrFib[num-1])
 
 #print(arrFib)
-countFirstDigit(arrFib)
+graphResults(countFirstDigit(arrFib))
 end = datetime.now()
 
 print("It took %.3f seconds"%((end - start).total_seconds()))
