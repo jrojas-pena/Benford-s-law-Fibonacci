e4eb3d83244530e29aa32d2b1defe27b7f9b392d
Juan <rojasjuandiego6@gmail.com>
2023-02-27 11:22:42 -0500

    Remove timers

--- BenfordsLaw.py
+++ BenfordsLaw.py
@@ -8,13 +8,10 @@ import numpy as np
 
 # Will return an array with the numbers in the Fibonacci sequence of size of num
 def fibonacci(arr, num):
-    start2 = datetime.now()
     arr.append(1)
     arr.append(1)
     for i in range(2, num):
         arr.append(arr[i-2] + arr[i -1])    
-    end2 = datetime.now()
-    print("On fibonacci funct %.03f"%(end2 - start2).total_seconds())
     return arr
 
     
@@ -23,15 +20,13 @@ def countFirstDigit(arr):
     # Dictionary of digits
     digitsDict = {}
 
-    start1 = datetime.now()
     for item in arr:    
         firstDigit = first_n_digits(item, 1)
         if firstDigit in digitsDict:
             digitsDict[firstDigit] += 1
         else:
             digitsDict[firstDigit] = 1
-    end1 = datetime.now()
-    print("On first Digit funct %.03f"%(end1 - start1).total_seconds())
+
     digitsDict = sorted(digitsDict.items(), key=lambda x: x[1], reverse=True)
     for i in digitsDict:
         print("% d : % d"%(i[0], i[1]))
@@ -52,13 +47,8 @@ def graphResults(digitsDict):
 arrFib = []
 
 num = int(input("Enter a number:"))
-start = datetime.now()
-
 arrFib = fibonacci(arrFib, num)
 #print(arrFib[num-1])
 
 #print(arrFib)
 graphResults(countFirstDigit(arrFib))
-end = datetime.now()
-
-print("It took %.3f seconds"%((end - start).total_seconds()))
