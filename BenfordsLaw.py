2b9dc31e4f9482f972a087246f1767eeb3ab8a52
Juan <rojasjuandiego6@gmail.com>
2023-03-05 11:44:00 -0500

    Memory optimization

--- BenfordsLaw.py
+++ BenfordsLaw.py
@@ -6,29 +6,43 @@ import numpy as np
 
 # Will return an array with the numbers in the Fibonacci sequence of size of num
 def fibonacci(arr, num):
-    arr.append(1)
-    arr.append(1)
-    for i in range(2, num):
-        arr.append(arr[i-2] + arr[i -1])    
-    return arr
-
-    
-# Counts the amount of times a specific digit appears as the first digit on the numbers inside the array (arr)
-def countFirstDigit(arr):
-    # Dictionary of digits
     digitsDict = {}
-
-    for item in arr:    
-        firstDigit = first_n_digits(item, 1)
+    arr.append(1)
+    arr.append(1)
+    next = 0
+    for i in range(2, num + 2):
+        firstDigit = first_n_digits(arr[0], 1)
         if firstDigit in digitsDict:
             digitsDict[firstDigit] += 1
         else:
             digitsDict[firstDigit] = 1
-
+        next = arr[0] + arr[1]
+        arr[0] = arr[1]
+        arr[1] = next  
+    
+    
     digitsDict = sorted(digitsDict.items(), key=lambda x: x[1], reverse=True)
     for i in digitsDict:
         print("% d : % d"%(i[0], i[1]))
     return digitsDict
+
+    
+# # Counts the amount of times a specific digit appears as the first digit on the numbers inside the array (arr)
+# def countFirstDigit(arr):
+#     # Dictionary of digits
+#     digitsDict = {}
+
+#     for item in arr:    
+#         firstDigit = first_n_digits(item, 1)
+#         if firstDigit in digitsDict:
+#             digitsDict[firstDigit] += 1
+#         else:
+#             digitsDict[firstDigit] = 1
+
+#     digitsDict = sorted(digitsDict.items(), key=lambda x: x[1], reverse=True)
+#     for i in digitsDict:
+#         print("% d : % d"%(i[0], i[1]))
+#     return digitsDict
 
 # Returns the first n digits in num
 def first_n_digits(num, n):
@@ -45,5 +59,8 @@ def graphResults(digitsDict):
 arrFib = []
 
 num = int(input("Enter a number:"))
+start = datetime.now()
 arrFib = fibonacci(arrFib, num)
-graphResults(countFirstDigit(arrFib))
+#countFirstDigit(arrFib)
+end = datetime.now()
+print("It took %.03f seconds"%((end-start).total_seconds()))
